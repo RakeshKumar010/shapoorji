f5e68b429c37d817d7a6c4fd2f7bb49b2b7531b9
rakesh <rakeshkumar748844@gmail.com>
2024-06-11 13:20:26 +0530

    add-some

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
   const {about}=useContext(Mycontext)
 
   return (
-    <div ref={about} className="flex flex-col py-10 lg:items-center gap-5 lg:gap-10 bg-white lg:text-center w-[90vw] lg:w-[70vw] mx-auto">
+    <div ref={about} className="flex flex-col py-10 lg:items-center gap-5 lg:gap-10 bg-white lg:text-center w-full p-5 lg:w-[70vw] mx-auto">
       <img src={Vanaha} alt="..." className="lg:h-32 h-24" />
       <p className="lg:text-4xl text-3xl ">Modern homes in a lush green sanctuary</p>
       <p className="text-teal-500 border border-teal-500 w-max p-1 rounded-full px-3">
--- src/components/globle/NavBar.jsx
+++ src/components/globle/NavBar.jsx
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Logo from "../../assets/logo.svg";
 import Menu from "../../assets/menu.png";
 import { Mycontext } from "../../App";
 
 const NavBar = () => {
+  const [openMob,setOpenMob]=useState(false)
   const topSpace = 100;
   const scrollToRef = (elementRef) => {
     window.scrollTo({
@@ -24,90 +25,170 @@ const NavBar = () => {
   } = useContext(Mycontext);
 
   return (
-    <div className="flex items-center z-20 justify-between shadow-xl h-20 px-5 lg:px-16 fixed left-0 right-0 top-0 bg-white">
-      <img
-        src={Logo}
-        alt=".."
-        className="lg:w-auto w-48"
-        onClick={() => {
-          scrollToRef(home);
-        }}
-      />
-      <div className="hidden gap-5 lg:flex">
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
+    <div className="fixed left-0 right-0 top-0">
+      <div className="flex items-center z-20 justify-between shadow-xl h-20 px-5 lg:px-16  bg-white">
+        <img
+          src={Logo}
+          alt=".."
+          className="lg:w-auto w-48"
           onClick={() => {
-            scrollToRef(about);
+            scrollToRef(home);
           }}
-        >
-          About
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(gallery);
-          }}
-        >
-          Gallery
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(floor);
-          }}
-        >
-          Floor Plans
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(emi);
-          }}
-        >
-          EMI Calculator
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(amenities);
-          }}
-        >
-          Amenities
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(location);
-          }}
-        >
-          Location
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(rera);
-          }}
-        >
-          RERA
-        </p>
-        <p
-          className="cursor-pointer text-sm hover:text-[#034ea2]"
-          onClick={() => {
-            scrollToRef(contact);
-          }}
-        >
-          Contact
-        </p>
-      </div>
-      <div className=" flex gap-10 items-center">
-        <span className="bg-[#034ea2] hidden lg:block text-white text-[12px] p-2  px-5 rounded-full">
-          Enquire
-        </span>
-        <div className="flex flex-col justify-center items-center ">
-          <img src={Menu} alt="" className="h-6" />
-          <p className="text-[12px]">Menu</p>
+        />
+        <div className="hidden gap-5 lg:flex">
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(about);
+            }}
+          >
+            About
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(gallery);
+            }}
+          >
+            Gallery
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(floor);
+            }}
+          >
+            Floor Plans
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(emi);
+            }}
+          >
+            EMI Calculator
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(amenities);
+            }}
+          >
+            Amenities
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(location);
+            }}
+          >
+            Location
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(rera);
+            }}
+          >
+            RERA
+          </p>
+          <p
+            className="cursor-pointer text-sm hover:text-[#034ea2]"
+            onClick={() => {
+              scrollToRef(contact);
+            }}
+          >
+            Contact
+          </p>
+        </div>
+        <div className=" flex gap-10 items-center">
+          <span className="bg-[#034ea2] hidden lg:block text-white text-[12px] p-2  px-5 rounded-full">
+            Enquire
+          </span>
+          <div className="flex flex-col justify-center items-center cursor-pointer" onClick={()=>{
+            setOpenMob(!openMob)
+          }}>
+            <img src={Menu} alt="" className="h-6" />
+            <p className="text-[12px]">Menu</p>
+          </div>
         </div>
       </div>
+
+     {openMob? <div
+        style={{
+          background:
+            "transparent linear-gradient(180deg,#063e92,#1589cf) 0 0 no-repeat padding-box",
+          height: "100vh",
+        }}
+        className="p-5"
+      >
+        <div className="gap-5 flex md:items-start items-center flex-col text-white ">
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(about);
+            }}
+          >
+            About
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(gallery);
+            }}
+          >
+            Gallery
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(floor);
+            }}
+          >
+            Floor Plans
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(emi);
+            }}
+          >
+            EMI Calculator
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(amenities);
+            }}
+          >
+            Amenities
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(location);
+            }}
+          >
+            Location
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(rera);
+            }}
+          >
+            RERA
+          </p>
+          <p
+            className="cursor-pointer text-lg hover:font-bold"
+            onClick={() => {
+              scrollToRef(contact);
+            }}
+          >
+            Contact
+          </p>
+        </div>
+      </div>:null}
     </div>
   );
 };
